8479af7ccf350d3b31c6e27e1697bfea4e76b7c3
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2022-11-21 17:03:06 -0300

    Pages

--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './About.scss';
+
 const About = () => {
   return (
     <div>About</div>
--- client/src/container/Testimonial/Testimonial.jsx
+++ client/src/container/Testimonial/Testimonial.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './Testimonial.scss';
+
 const Testimonial = () => {
   return (
     <div>Testimonial</div>
